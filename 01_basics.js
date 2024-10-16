42d304abbc626c7953de5992590a61d10265278b
Ujjwal Tiwari <ujjwaltiwari153@gmail.com>
2024-10-16 16:02:47 +0530

    Updated 01_basics.js with new functionality

--- 01_basics.js
+++ 01_basics.js
@@ -1,14 +1,29 @@
-const y = 15;
-// y = 25;  // Error: doesnt allow reassignment
+const fullName = "John Doe";
+// name= 25;  Error: doesnt allow reassignment
 
-let x = 10;
-x = 20;  // Valid, x is now 20....reassignment allow
+let age = 10;
+age = 20;  // valid
 
-let age = 23; // or const age = 23;
-age=20;
-console.log(y);// 15
-console.log(x);// 20
-console.log(age);// 20
+let city = "Mumbai"; // 
+city="Delhi";  // Valid, but not recommended.
+
+let email;
+
+console.log(fullName);// 
+console.log(age);// 
+console.log(city);//
+console.table([fullName, age, city,email]);// [10, 15, 20] tabular form
+
+ /*var age = 23;.....javascript mi ab hm var nhi use karte hai
+ var sirf function ke andar scoped hota hai, iska matlab agar 
+ aap var ko kisi loop ya block (if, for, while, etc.) ke andar declare karte ho,
+  toh us block ke bahar bhi wo variable accessible hoga....
+  if (true) {
+  var x = 10;
+}
+console.log(x);  // Output: 10 (x is accessible outside the block)
+Problem: Yaha x block ke bahar bhi accessible hai, jabki aapko lagta tha ki yeh sirf if block ke andar accessible hoga.*/
 
 
- 
+// accountCity="Mumbai";//bina variable a aage likhe v uska memory reserve kr sakte hai ..but its not a good practice.
+
