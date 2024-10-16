94e06a0286643daef865439b74115fe49417c3d6
Ujjwal Tiwari <ujjwaltiwari153@gmail.com>
2024-10-16 15:20:22 +0530

    First commit

---
+++ 01_basics.js
@@ -0,0 +1,14 @@
+const y = 15;
+// y = 25;  // Error: doesnt allow reassignment
+
+let x = 10;
+x = 20;  // Valid, x is now 20....reassignment allow
+
+let age = 23; // or const age = 23;
+age=20;
+console.log(y);// 15
+console.log(x);// 20
+console.log(age);// 20
+
+
+ 
