86d56628831449867ce229dcb03efb20f4504675
Ujjwal Tiwari <ujjwaltiwari153@gmail.com>
2024-10-17 22:36:44 +0530

    String updates

--- 06_String.js
+++ 06_String.js
@@ -39,4 +39,33 @@ console.log(text.includes("fun")); // true
 console.log(text.includes("abc")); // false
 console.log(text.charAt(4));     // S
 console.log(text.indexOf("n")); // 16
+const newString=text.substring(0,4);//0 se 3 index tak ka value ayega
+console.log(newString);//Java
+
+const anotherString=text.slice(0,4)//slice mi negative value v de sakte hai
+console.log(anotherString);
+
+let str = "JavaScript";
+
+// Using negative start index
+console.log(str.slice(-5)); // "cript" (starts from 5th last character)
+
+// Using negative start and end index
+console.log(str.slice(-5, -1)); // "crip" (from 5th last to 2nd last)
+
+
+//****************trim and replace method****************
+
+const str2 = " hello world ";
+
+console.log(str2);
+console.log(str2.trim()); // "hello world".....trim works on white spaces
+console.log(str2.trimStart()); // "hello world  "
+console.log(str2.trimEnd()); // "  hello world"
+
+const url ="https://ujjwal.com/hitesh%20kumar";
+console.log(url.replace('%20','-'));//https://ujjwal.com/hitesh-kumar
+console.log(url.includes('ujjwal'));
+
+console.log(str2.split(" "));//["", "hello", "world", ""]
 
