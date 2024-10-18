f320296257485fad6b87d3cfdb8e0f687552dd21
Ujjwal Tiwari <ujjwaltiwari153@gmail.com>
2024-10-18 08:48:54 +0530

    Numbers

---
+++ 07_number_maths.js
@@ -0,0 +1,8 @@
+const score=400;
+const bonus=50;
+const total=score+bonus;
+console.log(total)
+console.log(typeof score);//number..Js by default score ko number liya hai..but hm manually v mention kr sakte hai
+
+const balance = new Number(100);
+console.log(balance);//[Number: 100]
