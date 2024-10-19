9227e53eba81b128110af0a3f57a1622ab552dad
Ujjwal Tiwari <ujjwaltiwari153@gmail.com>
2024-10-19 23:37:13 +0530

    Object updated

--- 10_Objects.js
+++ 10_Objects.js
@@ -24,4 +24,20 @@ let person = {
   
   let newObj = Object.create(prototypeObj);
   console.log(newObj.greet());  // Output: Hello!
+
+  //another example of object literals
+
+  const newUser={
+    name:"Ujjwal",
+    age:21,
+    email:"ujjwal@123.com",
+    isLoggedIn:true,
+    isAdmin:false,
+    lastLoginDays :["Monday","Tuesday","Wednesday"]
+  }
+
+  //ways to access properties of object
+  console.log(newUser.email);//dot notation
+  console.log(newUser["email"]);//bracket notation..Bracket notation is useful 
+  //when property names have spaces or special characters, or if the property name is dynamically assigned
   
