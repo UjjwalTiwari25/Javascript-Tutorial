acb10ab4e68df7a9c7fbfe0069662d5ce68881a8
Ujjwal Tiwari <ujjwaltiwari153@gmail.com>
2024-10-23 18:19:16 +0530

    scope updates

---
+++ 12_Scope.js
@@ -0,0 +1,32 @@
+// let a= 10
+// const b= 20
+// var c= 30
+
+// console.log(a)
+// console.log(b)
+// console.log(c)
+
+//yeha tak koi dikkat nhi tha 
+
+// {
+//     scope ka concept yehi se ata hai
+// }
+
+let a= 102  //iska global scope hai
+ if (true){
+    let a= 10
+    const b= 20
+    var c= 30
+    console.log("inside if",a)
+
+}
+console.log(a)//.....ye thik hai
+// console.log(b)...ye thik hai
+console.log(c) //dikkat isme ata hai..ye var block k bahar v accssible hota hai
+
+
+//****for loop scope********************** */
+for (let i = 0; i < array.length; i++) {
+    const element = array[i];
+    
+}//isme i block scope k andar he hai
