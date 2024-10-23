24b74f569896d7fa81cf2a14003c8eff6b6d029c
Ujjwal Tiwari <ujjwaltiwari153@gmail.com>
2024-10-23 13:15:27 +0530

    Function updates

--- 11_function.js
+++ 11_function.js
@@ -31,3 +31,63 @@ function addNumbers(a, b) {
     return result;
 }
 console.log(addNumbers(3, 5));
+
+//**************Arrow function********************************************************************* */
+
+//Arrow functions provide a concise way to write functions and they do not bind their own this.
+//  They are commonly used in callbacks and array methods
+
+/*const functionName = (parameters) => {
+    // code to be executed
+    return value; // optional
+};
+*/
+
+const greet2 = (name) => `Hello, ${name}!`;
+
+console.log(greet('Ujjwal')); // Output: Hello, Ujjwal!
+
+
+//*****************************Parameters and arguments**************************************************************** */
+//Parameters are the variables defined in the function signature.
+// Arguments are the values passed to the function when it is invoked.
+
+function greet(name) { // 'name' is a parameter
+    console.log(`Hello, ${name}`);
+}
+
+greet('Virat'); // 'Virat' is an argument
+
+
+//Default parameters
+
+//Default parameters allow you to initialize function parameters with default values 
+//if no arguments are passed or undefined is passed.
+
+function greet1(name = 'Guest') {
+    console.log(`Hello, ${name}`);
+}
+
+greet1(); // Output: Hello, Guest
+greet1('Ujjwal'); // Output: Hello, Ujjwal
+
+//another example
+function loginUserMessage(username){
+    return `${username} logged in.`
+}
+loginUserMessage('Ujjwal');//yeha function return hogya hai but hamne ise print karne ko to nhi bola na kahi
+console.log(loginUserMessage('Ujjwal'));
+console.log((loginUserMessage()));//undefined logged in....agar hmkoi value he pass na kre to oo undefined ata hai null nhi ata
+
+
+//we can use if else here
+function loginUserMessage1(username1){
+    if(username1 === undefined){
+        console.log("Please enter username");
+        return;
+    }
+    
+        return '${username1} just logged in.'
+}
+// console.log(loginUserMessage1('Ujjwal'));
+console.log(loginUserMessage1());
---
+++ tempCodeRunnerFile.js
@@ -0,0 +1,1 @@
+console.log(loginUserMessage1('Ujjwal'));
