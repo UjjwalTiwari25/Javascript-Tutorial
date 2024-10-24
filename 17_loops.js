4d5cfbe6772e054465bc4428b6ac711734ccd074
Ujjwal Tiwari <ujjwaltiwari153@gmail.com>
2024-10-24 20:00:48 +0530

    Loops updates

--- 17_loops.js
+++ 17_loops.js
@@ -25,11 +25,23 @@ for (let i = 0; i < 10; i++) {
 
 //example of nested loop
 
-for (let i = 0; i < 3; i++) {
+for (let i = 0; i < 5; i++) {
+    console.log(`outer loop = ${i}`);
     for (let j = 0; j < 3; j++) {
-        console.log(`i = ${i}, j = ${j}`);
+        console.log(`outer looop = ${i}, inner loop = ${j}`);
     }
 }
+
+//table of 1 to 10
+
+for (let i = 1; i <= 10; i++) {
+    console.log(`Table of  = ${i}`);
+    for (let j = 1; j <= 10; j++) {
+        console.log(i + " * " + j + " = " + i * j);
+    }
+}
+
+
 
 //pyramid pattern of height 5
 let rows = 5;  // Number of rows
@@ -46,7 +58,24 @@ for (let i = 1; i <= rows; i++) {
 
 
 //************************************Breaking Loop******************* */
-//using break and continue
+//using break
+for (let index = 1; index <=20; index++) {
+    if(index==7){
+        break;  //jaise he index ka value 7 ho rha ...hm line no.68 k scope k bahar 
+                // jump kr ja rhe aur kuchh uske aage execute nhi ho rha
+    }
+    console.log(`Value of i is ${index}`); //1,2,3,4,5,6...iske aage nhi print nhi hoga
+  
+}
+
+//using continue
+for (let index = 1; index <=20; index++) {
+    if(index==7){
+        continue;   //isem jaise he index ka value 7 hoga ..to 7 ko hm skip kr denge bakki 8 se fir continue kr denge
+    }
+    console.log(`Value of i is ${index}`); 
+  
+}
 
 
 
