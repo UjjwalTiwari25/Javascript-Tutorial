69a3db039a5fecf27683f03a9df334d5df26c8e2
Ujjwal Tiwari <ujjwaltiwari153@gmail.com>
2024-11-25 22:51:10 +0530

    hello

--- 08_Classes_and _Oops/01_OopsLiterals.js
+++ 08_Classes_and _Oops/01_OopsLiterals.js
@@ -1,25 +1,39 @@
-const user = {     //yehi object literals hai
-    username:"Ujjwal",
+const user = {
+    username: "hitesh",
     loginCount: 8,
     signedIn: true,
 
-    /**
-     * Prints a message showing user's status.
-     * 
-     * The message shows the user's username and the number of times the user has logged in.
-     * 
-     * Example:
-     * getUserStatus() // "Username is Ujjwal and user has logged in 8 times"
-     */
-    getUserStatus: function(){
-        // console.log(`Username is ${this.username} and user has logged in ${this.loginCount} times`);
-            //console.log('Got user details from database')
-        console.log(this);
-    }
+    getUserDetails: function () {
+        console.log("User Details:");
+        console.log(`Username: ${this.username}`); // Access object properties using 'this'
+        // console.log(this); // Logs the object itself
+    },
+};
+
+// Accessing the object properties and method
+console.log(user.username); // Output: hitesh
+user.getUserDetails(); // Calls the method and logs user details
+
+
+//constructor
+function User(username, loginCount, isLoggedIn) {
+    // Define properties
+    this.username = username; // Property 1
+    this.loginCount = loginCount; // Property 2
+    this.isLoggedIn = isLoggedIn; // Property 3
+
+    // Define a method
+    this.greeting = function () {
+        console.log(`Welcome ${this.username}`); // Access properties using 'this'
+    };
 }
 
-console.log(user.username);
-console.log(user.getUserStatus());
-console.log(this);
+// Creating instances of the User constructor
+const userOne = new User("hitesh", 12, true);
+const userTwo = new User("ChaiAurCode", 11, false);
 
- 
+// Accessing the constructor and methods
+console.log(userOne.constructor); // Logs the constructor function
+console.log(userTwo); // Logs the userTwo object
+userOne.greeting(); // Output: Welcome hitesh
+userTwo.greeting(); // Output: Welcome ChaiAurCode
