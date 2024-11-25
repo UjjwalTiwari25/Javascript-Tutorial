8e07ff13b9850df1a46e89b14bfdb119b2bca652
Ujjwal Tiwari <ujjwaltiwari153@gmail.com>
2024-11-25 21:37:06 +0530

    hello

--- 08_Classes_and _Oops/01_OopsLiterals.js
+++ 08_Classes_and _Oops/01_OopsLiterals.js
@@ -1,12 +1,25 @@
-const user = {
+const user = {     //yehi object literals hai
     username:"Ujjwal",
     loginCount: 8,
     signedIn: true,
 
+    /**
+     * Prints a message showing user's status.
+     * 
+     * The message shows the user's username and the number of times the user has logged in.
+     * 
+     * Example:
+     * getUserStatus() // "Username is Ujjwal and user has logged in 8 times"
+     */
     getUserStatus: function(){
-        console.log(`Username is ${this.username} and user has logged in ${this.loginCount} times`);
+        // console.log(`Username is ${this.username} and user has logged in ${this.loginCount} times`);
+            //console.log('Got user details from database')
+        console.log(this);
     }
 }
 
 console.log(user.username);
 console.log(user.getUserStatus());
+console.log(this);
+
+ 
